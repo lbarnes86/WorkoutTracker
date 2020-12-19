ed54af54cd55a92feab6bfe85d73290d4b58641c
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-19 15:19:30 -0600

    added server.js file

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const logger = require("morgan")
+const mongoose = require("mongoose")
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+
+
+app.use(logger("dev"))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static("public"))
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://Mstarr:Flounder5@workout-tracker.7aafy.mongodb.net/workout"
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+
+app.use(require("./routes/apiroutes.js"))
+app.use(require("./routes/htmlroutes.js"))
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}.`)
+})
