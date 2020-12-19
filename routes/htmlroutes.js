5b08ebcafec15f9478aa9b8fc565449c69d14351
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-19 15:24:38 -0600

    added routes folder and apiroutes.js file

---
+++ routes/htmlroutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router()
+const db = require("../models")
+const path = require("path")
+
+// exercise
+router.use("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+// stats
+router.use("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+// index
+router.use("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+module.exports = router
